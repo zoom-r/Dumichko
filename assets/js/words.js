4e47683b193e6b1ffda779293ded7edfcebd64a6
Kristiyan Pulevski <kristiyanpulevski_j20@schoolmath.eu>
2024-03-28 08:17:52 +0200

    Changed word list

--- assets/js/words.js
+++ assets/js/words.js
@@ -1,16 +1,14 @@
 // Purpose: to fetch the words from the csv file and filter the words with 5 letters
 function fetchWords() {
-    const fiveLetterWords = [];
-    fetch('./assets/files/w_words.csv')
+    const words = [];
+    fetch('./assets/files/words.csv')
         .then(response => response.text())
         .then(data => {
-            const results = Papa.parse(data, { delimiter: ";", header: false });
+            const results = Papa.parse(data, { header: false });
             results.data.forEach(row => {
-                const word = row[1]; // Access the second column by its index
-                if (word && word.length == 5) {
-                    fiveLetterWords.push(word);
-                }
+                const word = row[0]; // Access the second column by its index
+                words.push(word);
             });
         });
-    return fiveLetterWords;
+    return words;
 }
