29283c62a8d259d6305495d1b712b3364f4fb32a
Radoslav Dimitrov <radodimitrov13@gmail.com>
2024-04-21 23:15:20 +0300

    Turn database connection into promise

--- back-end/database/conection.js
+++ back-end/database/conection.js
@@ -1,12 +1,18 @@
-const database = require('mysql2');
-database.createConnection({
-    host: 'dumichko-dumichko-5d95.j.aivencloud.com',
-    user: 'avnadmin',
-    password: 'AVNS_8RhJct6M7c627FM_TxD',
-    database: 'defaultdb',
-    port: '14843',
-    ssl: {
-        ca: 'ca.pem'
-    }
-});
-module.exports = database;
+import mysql from 'mysql2/promise';
+let connection;
+try{
+    connection = await mysql.createConnection({
+        host: 'dumichko-dumichko-5d95.j.aivencloud.com',
+        user: 'avnadmin',
+        password: 'AVNS_8RhJct6M7c627FM_TxD',
+        database: 'defaultdb',
+        port: '14843',
+        ssl: {
+            ca: 'ca.pem'
+        }
+    });
+}catch(err){
+    console.log(err);
+}
+
+module.exports = connection;
