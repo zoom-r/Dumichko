fff9180553bfe3806950c2716ab9cf08308d71ed
Radoslav Dimitrov <radodimitrov13@gmail.com>
2024-04-21 20:02:14 +0300

    Adding Login page route

--- back-end/index.js
+++ back-end/index.js
@@ -8,10 +8,13 @@ app.use(express.static(path.resolve(__dirname, '../front-end')));
 // Load routes
 const gameRouter = require('./routes/game');
 const homeRouter = require('./routes/home');
+const loginRouter = require('./routes/login');
 
 // Use routes
 app.use('/game', gameRouter);
 app.use('/', homeRouter);
+app.use('/login', loginRouter);
+
 
 app.all('*', (req, res) => {
     res.status(404).sendFile(path.resolve(__dirname, '../front-end/pages/404.html'));
