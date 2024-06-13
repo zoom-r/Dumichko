43919500931c969ee77ea12429b93b222dafc4c6
Kristiyan Pulevski <kristiyanpulevski_j20@schoolmath.eu>
2024-06-13 14:15:56 +0300

    tiles.js

---
+++ front-end/js/tiles.js
@@ -0,0 +1,13 @@
+function checkLetters(row){
+    let wordOfTheDay;
+    let wordOfTheDayLetters = wordOfTheDay.split();
+    let enteredWord;
+    let enteredWordLetters = enteredWord.split();
+    for(let i = 0; i < 6; i++){
+        if(enteredWordLetters[i] == wordOfTheDayLetters[i]){
+            
+        }
+    }
+}
+
+module.exports = checkLetters;
