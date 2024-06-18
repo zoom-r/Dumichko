551acef0c04a477d4fe43c43d129ed162c5b7f60
Radoslav Dimitrov <radodimitrov13@gmail.com>
2024-06-19 00:30:46 +0300

    checkPassword
    Integrated password checking for register

--- front-end/js/login.js
+++ front-end/js/login.js
@@ -3,7 +3,7 @@ async function checkEmail (form) {
         form.dataset.state = '';
         return;
     }
-    
+
     axios.post('/login/check-email', {
         email: form.value, 
         type: form.id
@@ -18,5 +18,27 @@ async function checkEmail (form) {
 }
 
 async function checkPassword (form){
-
+    if(form.id == 'signupPassword'){
+        if(form.value.length < 8 || form.value.length > 40 || /\d/.test(form.value) == false){
+            form.dataset.state = 'invalid';
+            document.getElementById('signupInfoPassword').innerHTML = '⚠ Паролата трябва да бъде 8-40 символа и да съдържа поне една цифра!';
+            document.getElementById('signupInfoPassword').style.color = 'red';
+            document.getElementById('signupRepeatPassword').disabled = true;
+            document.getElementById('signupRepeatPassword').value = '';
+            document.getElementById('signupInfoRepeatPassword').innerHTML = '';
+        }else{
+            form.dataset.state = 'valid';
+            document.getElementById('signupInfoPassword').innerHTML = '';
+            document.getElementById('signupRepeatPassword').disabled = false;
+        }
+    }else if(form.id == 'signupRepeatPassword'){
+        if(form.value !== document.getElementById('signupPassword').value){
+            form.dataset.state = 'invalid';
+            document.getElementById('signupInfoRepeatPassword').innerHTML = '⚠ Паролите не съвпадат!';
+            document.getElementById('signupInfoRepeatPassword').style.color = 'red';
+        }else{
+            form.dataset.state = 'valid';
+            document.getElementById('signupInfoRepeatPassword').innerHTML = '';
+        }
+    }
 }
